062e173fb97001717c1909be5e3300dcd7990e9c
Alexis <alexisvt@gmail.com>
2015-03-31 08:27:13 -0600

    initial commit

---
+++ Chapter6/Chapter6/Models/LinqValueCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chapter6.Models
+{
+    public class LinqValueCalculator: IValueCalculator
+    {
+        private IDiscountHelper discounter;
+
+        public LinqValueCalculator(IDiscountHelper discounterParam)
+        {
+            discounter = discounterParam;
+        }
+
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return discounter.ApplyDiscount(products.Sum(p => p.Price));
+        }
+    }
+}
