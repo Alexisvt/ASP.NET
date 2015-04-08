6802cec439e93125690c91e001fb6dd3f44c693e
Alexis <alexisvt@gmail.com>
2015-04-08 16:57:05 -0600

    adding more feautures

--- SportStore/SportStore.UnitTests/UnitTest1.cs
+++ SportStore/SportStore.UnitTests/UnitTest1.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+using SportStore.WebUI.Controllers;
 
 namespace SportStore.UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]{
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //Act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+        }
         [TestMethod]
         public void TestMethod1()
         {
--- SportStore/SportStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportStore/SportStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -33,13 +33,6 @@ namespace SportStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            //mock.Setup(m => m.Products).Returns(new List<Product> {
-            //    new Product {Name = "Football", Price = 25},
-            //    new Product {Name = "Surf board", Price = 179},
-            //    new Product {Name = "Running shoes", Price = 95}
-            //});
-
             kernel.Bind<IProductRepository>().To<EFProductRepostory>();
         }
     }
