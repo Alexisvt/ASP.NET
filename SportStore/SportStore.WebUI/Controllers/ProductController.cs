06b87b1ac16296172025f1cea91c488e9d4f0752
Alexis <alexisvt@gmail.com>
2015-04-15 11:53:49 -0600

    completing change on sportStore

--- SportStore/SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore/SportStore.WebUI/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using SportStore.Domain.Abstract;
 using SportStore.Domain.Entities;
+using SportStore.WebUI.Models;
 
 namespace SportStore.WebUI.Controllers
 {
@@ -20,10 +21,20 @@ namespace SportStore.WebUI.Controllers
 
         public ViewResult List(int page = 1)
         {
-            return View(repository.Products
-                .OrderBy( p => p.ProductID)
-                .Skip((page - 1) *  PageSize)
-                .Take(PageSize));
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
