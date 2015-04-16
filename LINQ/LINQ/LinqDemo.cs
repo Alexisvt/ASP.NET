c73503d4e4966a04c658d7257236c9b75cfdbf10
Alexis <alexisvt@gmail.com>
2015-04-16 08:26:43 -0600

    linq first commit

---
+++ LINQ/LINQ/LinqDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class LinqDemo
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Alexis", "Melissa", "David", "Margoth" };
+
+            IEnumerable<string> filteredNames = names.Where(n => n.Length >= 4);
+
+            foreach (string item in filteredNames)
+            {
+                Console.WriteLine(item);
+                Console.ReadKey();
+            }
+        }
+    }
+}
