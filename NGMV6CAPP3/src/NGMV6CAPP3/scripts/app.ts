65d4e7f9a3da39417f1f71955a4d81e4ebe25958
alexisvt <alexisvt@gmail.com>
2015-12-16 10:04:08 -0600

    adding dts

--- NGMV6CAPP3/src/NGMV6CAPP3/scripts/app.ts
+++ NGMV6CAPP3/src/NGMV6CAPP3/scripts/app.ts
@@ -1,5 +1,4 @@
-﻿///<reference path="../typings/angularjs/angular.d.ts"/>
-///<reference path="../typings/angularjs/angular-route.d.ts"/>
+﻿///<reference path="../typings/tsd.d.ts"/>
 
 ((): void => {
         angular.module("maintenance", ["ngRoute"]);
