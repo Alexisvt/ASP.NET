89e9ce5857c2b4ac5a9d15d07331916091bfd6e6
alexisvt <alexisvt@gmail.com>
2017-10-05 15:29:40 -0600

    initial commit

---
+++ MVCQuiz/MVCQuiz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCQuiz.Global" Language="C#" %>
---
+++ coreapp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace coreapp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WriteLine("Hello World!");
+
+      NumerosRecursivos(0);
+
+      ReadKey();
+
+    }
+
+    private static void NumerosRecursivos(int n)
+    {
+      if (n <= 100)
+      {
+
+        if ((n % 2 == 0) || (n % 5 == 0))
+        {
+          WriteLine(n);
+        }
+
+        n++;
+        NumerosRecursivos(n);
+      }
+    }
+  }
+}
