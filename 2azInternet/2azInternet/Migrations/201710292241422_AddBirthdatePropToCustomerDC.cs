754d4bcf8b6d8bc9eacb87c3013f77fb0a16941c
alexisvt <alexisvt@gmail.com>
2017-10-29 16:42:45 -0600

    Adding migration to add birthdate property and column to Customer domain class

---
+++ 2azInternet/2azInternet/Migrations/201710292241422_AddBirthdatePropToCustomerDC.cs
@@ -0,0 +1,18 @@
+namespace _2azInternet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBirthdatePropToCustomerDC : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Birthdate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "Birthdate");
+        }
+    }
+}
