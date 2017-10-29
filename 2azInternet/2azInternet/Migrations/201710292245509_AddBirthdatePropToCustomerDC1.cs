1b6a012943c8ea3bd84a7627e0fbb1f603100753
alexisvt <alexisvt@gmail.com>
2017-10-29 16:47:12 -0600

    Making nullable the datetime property and column in Customer

---
+++ 2azInternet/2azInternet/Migrations/201710292245509_AddBirthdatePropToCustomerDC1.cs
@@ -0,0 +1,18 @@
+namespace _2azInternet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBirthdatePropToCustomerDC1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Birthdate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Birthdate", c => c.DateTime(nullable: false));
+        }
+    }
+}
