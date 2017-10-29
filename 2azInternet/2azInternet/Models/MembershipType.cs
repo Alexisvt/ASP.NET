4e35fe4ce1b2a5da144feaeb296c72f382339bbf
alexisvt <alexisvt@gmail.com>
2017-10-29 15:08:58 -0600

    Add  name property to MembershipeType and add a migration of it

--- 2azInternet/2azInternet/Models/MembershipType.cs
+++ 2azInternet/2azInternet/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace _2azInternet.Models
     public class MembershipType
     {
         public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
