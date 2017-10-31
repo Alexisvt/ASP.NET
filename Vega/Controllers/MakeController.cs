7254b2818f5dc567b2fd8a7cc910ecdca6b83d1b
alexisvt <alexisvt@gmail.com>
2017-10-31 13:35:25 -0600

    adding Features endpoint and fix GetMakes endpoint

---
+++ Vega/Controllers/MakeController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vega.Models;
+
+namespace Vega.Controllers
+{
+  public class MakeController : Controller
+  {
+    private readonly VegaDbContext _context;
+
+    public MakeController(VegaDbContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet("/api/makes")]
+    public IActionResult GetMakes()
+    {
+      var makes = _context.Makes.Include(m => m.Models).ToList();
+
+      return Ok(makes);
+    }
+
+    [HttpGet("/api/features")]
+    public IActionResult GetFeatures()
+    {
+      var features = _context.Features.ToList();
+
+      return Ok(features);
+    }
+  }
+}
